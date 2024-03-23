a4306326c04089a55f8abcb3de85af1a3f7bdaa6
kellypride <ngehkellypride181@gmail.com>
2024-03-23 14:39:45 +0100

    Initial commit of project 2

---
+++ project-2/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import MainContent from './components/Main/MainContent';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <div class="App">
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
